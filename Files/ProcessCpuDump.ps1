6d495d01b1c09215ab5494c8a5ad0100b2327688
Gianluca De Rossi <gderossi@microsoft.com>
2025-02-12 18:52:11 +0100

    Enhance CPU usage dump script to include system information and calculate usage per core, excluding idle processes

--- Files/ProcessCpuDump.ps1
+++ Files/ProcessCpuDump.ps1
@@ -1,11 +1,14 @@
-$dump = Get-Process | Select-Object Name, Id, CPU, StartTime 
+$dump = Get-Process | Select-Object Name, Id, CPU, StartTime, SI 
 $Now=Get-Date
+$numCores = (Get-WmiObject -Class Win32_ComputerSystem).NumberOfLogicalProcessors
 $cpuUsage = @()
 foreach ($process in $dump) {
-    $Interval=$Now-$process.StartTime
-    $cpuUsage += [PSCustomObject]@{
-        Name = $process.Name
-        CPUUsagePercent = $process.CPU/$Interval.TotalSeconds*100
+    if ($process.Name -ne 'Idle') {
+        $Interval=$Now-$process.StartTime
+        $cpuUsage += [PSCustomObject]@{
+            Name = $process.Name
+            CPUUsagePercent = $process.CPU/$Interval.TotalSeconds/$numCores*100
+        }
     }
 }
 $cpuUsage | Sort-Object CPUUsagePercent -Descending | Select -First 5 | ConvertTo-Json
